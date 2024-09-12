4777b7c136d2f542dc3447639ec8041355c39943
jose gomez <josegomez120@gmail.com>
2024-09-12 16:43:50 -0500

    exceptions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
-import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -46,8 +46,12 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       where: { id: id }
     });
 
-    if( !product )
-      throw new NotFoundException(`Product with id ${ id } not found`);
+    if ( !product ) {
+      throw new RpcException({ 
+        message: `Product with id #${ id } not found`,
+        status: HttpStatus.BAD_REQUEST
+      });
+    }
     return product;
   }
 
